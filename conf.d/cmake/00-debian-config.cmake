15eeb417dabe30d6ec27b59822e34917f8486e90
José Bollo <jose.bollo@iot.bzh>
2024-03-27 13:19:37 +0100

    Refactor cmake build scripts

--- conf.d/cmake/00-debian-config.cmake
+++
@@ -1,4 +0,0 @@
-message(STATUS "Custom options: 00-debian-config.cmake --")
-
-# Libshell is not part of standard Linux Distro (https://libshell.org/)
-set(ENV{PKG_CONFIG_PATH} "$ENV{PKG_CONFIG_PATH}:/usr/local/lib64/pkgconfig")

cfb83d217557d04b437accb49b9605a2230359e7
Ronan Le Martret <ronan.lemartret@iot.bzh>
2026-09-07 08:38:14 +0200

    style: format simple API sample
    - fix the sample filename in the module documentation
    - apply consistent Python formatting and spacing
    - normalize dictionary quoting and layout
    - improve readability of verb and binder definitions

--- samples/simple-api.py
+++ samples/simple-api.py
@@ -5,7 +5,7 @@ Copyright 2021 Fulup Ar Foll fulup@iot.bzh
 Licence: $RP_BEGIN_LICENSE$ SPDX:MIT https://opensource.org/licenses/MIT $RP_END_LICENSE$
 
 object:
-    simple-api.python create a single api(demo) with two verbs 'ping' + 'args'
+    simple-api.py create a single api(demo) with two verbs 'ping' + 'args'
     this api can be requested from REST|websocket from a browser on http:localhost:1234
 
 usage
@@ -21,55 +21,70 @@ config: following should match your installation paths
 import libafb
 
 ## static variables
-count=0
+count = 0
+
 
 ## ping/pong test func
 def pingCB(rqt, *args):
     global count
     count += 1
-    return (0, {"pong":count}) # implicit response
+    return (0, {"pong": count})  # implicit response
+
 
 def argsCB(rqt, *args):
     libafb.notice(rqt, "actionCB query=%s", args)
-    libafb.reply (rqt, 0, {'query': args})
+    libafb.reply(rqt, 0, {"query": args})
+
 
 ## executed when binder is ready to serv
 def loopBinderCb(binder, nohandle):
     libafb.notice(binder, "loopBinderCb=%s", libafb.config(binder, "uid"))
-    return 0 # keep running for ever
+    return 0  # keep running for ever
+
 
 ## api verb list
 demoVerbs = [
-    {'uid':'py-ping', 'verb':'ping', 'callback':pingCB, 'info':'py ping demo function'},
-    {'uid':'py-args', 'verb':'args', 'callback':argsCB, 'info':'py check input query', 'sample':[{'arg1':'arg-one', 'arg2':'arg-two'}, {'argA':1, 'argB':2}]},
+    {
+        "uid": "py-ping",
+        "verb": "ping",
+        "callback": pingCB,
+        "info": "py ping demo function",
+    },
+    {
+        "uid": "py-args",
+        "verb": "args",
+        "callback": argsCB,
+        "info": "py check input query",
+        "sample": [{"arg1": "arg-one", "arg2": "arg-two"}, {"argA": 1, "argB": 2}],
+    },
 ]
 
 ## define and instantiate API
 demoApi = {
-    'uid'     : 'py-demo',
-    'api'     : 'demo',
-    'class'   : 'test',
-    'info'    : 'py api demo',
-    'verbose' : 9,
-    'export'  : 'public',
-    'verbs'   : demoVerbs,
+    "uid": "py-demo",
+    "api": "demo",
+    "class": "test",
+    "info": "py api demo",
+    "verbose": 9,
+    "export": "public",
+    "verbs": demoVerbs,
 }
 
 # define and instantiate libafb-binder
 demoOpts = {
-    'uid'     : 'py-binder',
-    'port'    : 1234,
-    'verbose' : 9,
-    'rootdir' : '.',
+    "uid": "py-binder",
+    "port": 1234,
+    "verbose": 9,
+    "rootdir": ".",
 }
 
 # instantiate binder and API
-binder= libafb.binder(demoOpts)
+binder = libafb.binder(demoOpts)
 myapi = libafb.apiadd(demoApi)
 
 # enter loopstart
-status= libafb.loopstart(binder, loopBinderCb)
-if status < 0 :
-    libafb.error (binder, "OnError loopstart Exit")
+status = libafb.loopstart(binder, loopBinderCb)
+if status < 0:
+    libafb.error(binder, "OnError loopstart Exit")
 else:
     libafb.notice(binder, "OnSuccess loopstart Exit")
